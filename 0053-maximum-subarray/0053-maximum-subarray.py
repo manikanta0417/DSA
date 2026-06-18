8a5c6abd159181eb3119b7aacb5f488dbd914974
Rallacharla Manikanta <manirallacharla@gmail.com>
2026-06-18 18:51:33 +0530

    Time: 35 ms (89.59%), Space: 21.1 MB (72.09%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        max=float("-inf")
+        sum=0
+        for i in range(len(nums)):
+            sum+=nums[i]
+            if sum>max:
+                max=sum
+            if sum<0:
+                sum=0
+        return max
+                
+
+
+        
